831c1a3f3a9778a8430702f9e60e70169dc21369
mb <minhcf2013@gmail.com>
2020-12-06 23:19:20 +0900

    Change graphql and s3 endpoints

--- book-web2/helpers/generateImageUrl.ts
+++ book-web2/helpers/generateImageUrl.ts
@@ -1,4 +1,5 @@
 const generateImageURL = (fileName: string): string => {
-  return "https://book-server-image.s3-ap-northeast-1.amazonaws.com/" + fileName;
+  const endpoint = "https://book-server-image.s3-ap-northeast-1.amazonaws.com/";
+  return endpoint + fileName;
 };
 export { generateImageURL };
--- book-web2/lib/apolloClient.tsx
+++ book-web2/lib/apolloClient.tsx
@@ -9,7 +9,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "http://localhost:4000/graphql", // Server URL (must be absolute)
+      uri: "http://54.178.4.28/graphql", // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache(),
